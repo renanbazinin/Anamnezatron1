72f6cd8190dfbd6b688f69fa4416477e293d189c
renanbazinin <renanbazinin2@gmail.com>
2019-10-23 22:53:57 +0300

    8.1

--- main.js
+++ main.js
@@ -190,6 +190,17 @@ function GlobalAdd(Index, kind) {
 
         document.getElementById("anam").innerHTML += textfortext;
 
+        var textfortext = CreateAnaText();
+        
+        var copyText = document.getElementById("anam");
+
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+      
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+
     }
 }
 function BackIndex(Index){
